56131e63e3b223b069267191eede13c2261c81ee
Denis <denkingbad@gmail.com>
2024-01-10 16:00:54 +0700

    add requirenments.txt and test_python.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-Clear all
+import time
+
+from selenium import webdriver
+
+driver = webdriver.Chrome('C:\Users\GIGACHAD\PycharmProjects\selenium_with_python\driver\chrome.exe')
+url = "http://www.google.com"
+
+driver.get(url)
+
+time.sleep(5)  # Let the user actually see something!
+
+search_box = driver.find_element_by_name('q')
+
+search_box.send_keys('ChromeDriver')
+
+search_box.submit()
+
+time.sleep(5)  # Let the user actually see something!
+
+driver.quit()
